642628d3c62c0d8b724addc3d608703685dc12ef
Anderson Rosa de Almeida <anderson.almeida@pmweb.com.br>
2016-03-08 11:41:35 -0300

    verify either if enter or exit the viewport

--- scrollManager.js
+++ scrollManager.js
@@ -1,36 +1,61 @@
 (function(factory) {
 	factory(window, document, jQuery);
 }(function(win, doc, $) {
+	'use strict';
 
 	var ScrollManager = function(element, options) {
+		this.element = element;
 		this.elmHeight = element.height();
 		this.elmWidth = element.width(); 
 		this.context = options.context;
 		this.onEnter = options.onEnter;
 		this.onExit = options.onExit;
+		this.inView = false;
 		
 		// bind events		
 		this.binds();
+		this.isInView();
 	};
 
 	ScrollManager.prototype.binds = function() {
 		var self = this;
 
 		$(this.context).on('scroll', function() {
-			console.log("scrollando as parada tudo !!!!");
-			
-			if( self.isInView() ) {
 
-				if(self.onEnter) {
-					self.onEnter.call(this);
-				}
+			self.isInView();
 
-			}
 		});
 	};
 
 	ScrollManager.prototype.isInView = function() {
-		
+		var sec = this.element,
+			context = $(this.context),
+			elmHeight = this.elmHeight,
+			winOffset = context.scrollTop() + win.innerHeight;		
+
+		// verifica se entrou na viewport
+		if( !this.inView && winOffset > sec.offset().top && winOffset <= ( sec.offset().top + sec.height() ) ) {
+			this.inView = true;
+
+			if(this.onEnter) {
+				this.onEnter.call(this, sec);
+			}
+
+			return true;
+		}
+
+		// verifica se saiu da viewport
+		if( this.inView && winOffset < sec.offset().top || this.inView && winOffset > ( sec.offset().top + sec.height() ) ) {
+			this.inView = false;
+
+			if(this.onExit) {
+				this.onExit.call(this, sec);
+			}
+
+			return false;
+		}	
+
+
 		return false;
 	};
 
